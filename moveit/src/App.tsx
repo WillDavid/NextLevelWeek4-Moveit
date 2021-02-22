985b9f65e5097c5fb4ebb77dc4ddc43d8b9afdbf
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-02-22 14:09:15 -0400

    finish experience bar

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -1,21 +1,12 @@
 
-import {Button} from './components/Button';
+import './styles/global.css';
+import {ExperienceBar} from './components/ExperienceBar'
 
 function App() {
   return (
-      <div>
-      <Button color='red'>
-        Botão 1
-      </Button>
-      <Button color='blue'>
-        Botão 2
-      </Button>
-      <Button color='green'>
-        Botão 3
-      </Button>
-
+      <div className="container">
+        <ExperienceBar></ExperienceBar>
       </div>
-    
     
   )
 }
