370cb3e4f01b0c50839ba54f20669c6bb6d6559e
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-02-24 11:01:16 -0400

    att index

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { CompletedChallenges } from "../components/CompletedChallenges";
-import { Countdown } from "../components/CountDown";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
 import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
+import { ChallengeBox } from "../components/ChallengeBox";
 
 export default function Home() {
   return (
@@ -22,6 +23,8 @@ export default function Home() {
             <Countdown></Countdown>
           </div>
           <div>
+
+            <ChallengeBox></ChallengeBox>
             
 
           </div>
