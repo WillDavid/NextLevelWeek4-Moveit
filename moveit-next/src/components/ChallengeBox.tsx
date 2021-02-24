9a51c0226b4f0fbea74fc7efb3adaa483baf48b7
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-02-24 10:58:03 -0400

    countdown finished

---
+++ moveit-next/src/components/ChallengeBox.tsx
@@ -0,0 +1,8 @@
+import styles from '../styles/components/ChallengeBox.module.css'
+export function ChallengeBox(){
+    return(
+        <div className={styles.challengeBoxContainer}>
+            
+        </div>
+    )
+}
