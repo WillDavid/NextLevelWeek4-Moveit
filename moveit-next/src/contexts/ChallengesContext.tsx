e13c17d7283b0a1e0334c56bbc653decfd6723e4
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-02-26 10:35:56 -0400

    add cookies functions

--- moveit-next/src/contexts/ChallengesContext.tsx
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useState, ReactNode, useEffect } from "react";
-
+import Cookies from 'js-cookie'
 import challenges from '../../challenges.json'
 
 
@@ -45,7 +45,17 @@ export function ChallengesProvider({children} : ChallengesProviderProps){
         Notification.requestPermission()
     }, []
     )
-  function levelUp(){
+
+    useEffect(( ) => {
+        Cookies.set('level', String(level));
+        Cookies.set('currentExperience', String(currentExperience));
+        Cookies.set('challengesCompleted', String(challengesCompleted));
+
+    }, [level, currentExperience, challengesCompleted])
+  
+  
+  
+    function levelUp(){
     setLevel(level + 1)
     
   }
